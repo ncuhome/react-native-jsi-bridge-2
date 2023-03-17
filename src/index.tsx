d081763324f20bbdf5bbcef0b03befc89c0d481f
Kuss <kussssss@outlook.com>
2023-03-17 22:33:30 +0800

    rename: react-native-jsi-bridge-2

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
-  `The package 'react-native-jsi-bridge' doesn't seem to be linked. Make sure: \n\n` +
+  `The package 'react-native-jsi-bridge-2' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
